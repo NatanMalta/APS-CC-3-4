e24dc0b37dfbed6e4d8a8da91737c2ed796837bb
NatanMalta <39250455+NatanMalta@users.noreply.github.com>
2020-11-15 16:56:57 -0300

    pequeno bug

--- src/aps_dados/model/Insertion.java
+++ src/aps_dados/model/Insertion.java
@@ -15,7 +15,7 @@ public class Insertion {
     public static void sort(List<Comparable> a) {
         int n = a.size();
         if (n == 1) return;
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             for(int j = (i - 1); (j > 0 && Util.menor(a.get(j), a.get(j - 1))); j--) {
                 Util.trocar(a, j, j - 1);
             }
